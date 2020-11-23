2f09676b7a7d9114519a7da28a78af2be0003485
xiongben <xiongben1993@gmail.com>
2020-11-23 18:13:30 +0800

    init

---
+++ compiled.js
@@ -0,0 +1,61 @@
+function view() {
+    return h(
+        "ul",
+        { id: "filmList", className: "list" },
+        h(
+            "li",
+            { className: "main" },
+            "Detective Chinatown Vol 2"
+        ),
+        h(
+            "li",
+            null,
+            "Ferdinand"
+        ),
+        h(
+            "li",
+            null,
+            "Paddington 2"
+        )
+    );
+}
+
+function flatten(arr) {
+    return [].concat(...arr);
+}
+
+function h(type, props, ...children) {
+    return {
+        type,
+        props: props || {},
+        children: flatten(children)
+    };
+}
+
+function createElement(node) {
+    if (typeof node === "string") {
+        return document.createTextNode(node);
+    }
+    let { type, props, children } = node;
+    const el = document.createElement(type);
+    setProps(el, props);
+    children.map(createElement).forEach(el.appendChild.bind(el));
+    return el;
+}
+
+function setProp(target, name, value) {
+    if (name == "className") {
+        return target.setAttribute('class', value);
+    }
+    target.setAttribute(name, value);
+}
+
+function setProps(target, props) {
+    Object.keys(props).forEach(key => {
+        setProp(target, key, props[key]);
+    });
+}
+
+function render(el) {
+    el.appendChild(createElement(view()));
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+function view() {
+    return <ul id="filmList" className="list">
+      <li className="main">Detective Chinatown Vol 2</li>
+      <li>Ferdinand</li>
+      <li>Paddington 2</li>
+    </ul>
+  }
+
+
+function flatten(arr){
+    return [].concat(...arr);
+}
+
+
+function h(type, props, ...children) {
+    return {
+      type,
+      props: props || {},
+      children: flatten(children)
+    }
+  }
+
+function createElement(node){
+   if(typeof(node) === "string"){
+       return document.createTextNode(node);
+   }
+   let { type, props, children } = node;
+   const el = document.createElement(type)
+   setProps(el, props)
+   children.map(createElement).forEach(el.appendChild.bind(el));
+   return el;
+}
+
+function setProp(target,name,value){
+    if(name == "className"){
+        return target.setAttribute('class',value);
+    }
+    target.setAttribute(name,value);
+}
+
+function setProps(target, props){
+    Object.keys(props).forEach(key=>{
+        setProp(target,key,props[key]);
+    })
+}
+
+
+function render(el) {
+  el.appendChild(createElement(view()))
+}
+
+  
