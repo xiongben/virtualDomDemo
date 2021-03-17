ec3ca6641f26efe0f019cb1a675232151dbe5b80
xiongben <xiongben1993@gmail.com>
2021-03-17 18:23:09 +0800

    [add] diff dom demo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,37 @@ import patch from "./mysnabbdom/patch"
 
 // const myVnode1 = h('h1',{}, 'hello xiongben');
 
-const myVnode1 = h('ul',{}, [
-    h('li',{},"A"),
-    h('li',{},"B"),
-    h('li',{},"C"),
-    h('li',{},"D"),
-]);
+const btn = document.getElementById('btn');
+
+
+// const myVnode1 = h('ul',{}, [
+//     h('li',{},"A"),
+//     h('li',{},"B"),
+//     h('li',{},[
+//         h('div',{},[
+//             h('ol',{},"aa"),
+//             h('ol',{},"bb"),
+//             h('ol',{},"cc"),
+//         ])
+//     ]),
+//     h('li',{},"D"),
+// ]);
+
+const myVnode1 = h('section',{}, "oldtextjjjjjjjj");
 
 const container = document.getElementById('app');
 
-
 patch(container, myVnode1);
+
+const myVnode2 = h('section',{}, [
+    h('p',{},"A2"),
+    h('p',{},"B"),
+    h('p',{},"D"),
+]);
+
+btn.onclick = function (){
+    patch(myVnode1, myVnode2);
+}
+
+
+
--- src/mysnabbdom/patch.js
+++ src/mysnabbdom/patch.js
@@ -8,8 +8,29 @@ export default function (oldVode, newVnode){
         oldVode = vnode(oldVode.tagName.toLocaleLowerCase(),{},[],undefined, oldVode);
     }
     //判断oldVnode and newVnode 是不是同一节点
-    if((oldVode.key)&&(oldVode.key == newVnode.key && oldVode.sel == newVnode.sel)){
+    if(oldVode.key == newVnode.key && oldVode.sel == newVnode.sel){
         console.log("same node");
+        if(oldVode === newVnode) return ;
+        if(newVnode.text != undefined && (newVnode.children == undefined || newVnode.children.length == 0)){
+            console.log("new vnode has text attr");
+            if(newVnode.text != oldVode.text){
+                //新的节点的text和旧的节点的text不同
+                oldVode.elm.innerText = newVnode.text;
+            }
+        }else{
+            //newVnode没有text属性
+            if(oldVode.children != undefined && oldVode.children.length > 0){
+                //新旧节点都有children
+
+            }else {
+                //旧的节点没有children，新的节点有children
+                oldVode.elm.innerHTML = "";  //清空旧的节点的内容
+                for (let i = 0 ; i < newVnode.children.length; i++){
+                    let dom = createElement(newVnode.children[i]);
+                    oldVode.elm.appendChild(dom);
+                }
+            }
+        }
     }else{
         console.log("different node,insert new dom,delete old dom");
         console.log(oldVode, newVnode)
@@ -17,6 +38,8 @@ export default function (oldVode, newVnode){
         if(oldVode.elm.parentNode && newVnodeDomElm){
             oldVode.elm.parentNode.insertBefore(newVnodeDomElm, oldVode.elm);
         }
+        //删除旧的节点
 
+        oldVode.elm.parentNode.removeChild(oldVode.elm);
     }
 }
